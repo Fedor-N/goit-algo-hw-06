61399e306bfa0c223edcf5f07cc6a48545e25058
Fedor-N <nfedorf@gmail.com>
2024-01-29 17:28:22 +0200

    Create bfs_algorithm.py

---
+++ bfs_algorithm.py
@@ -0,0 +1,11 @@
+# bfs_algorithm.py
+def bfs(graph, start, end):
+    queue = [(start, [start])]
+    while queue:
+        (node, path) = queue.pop(0)
+        for next_node in graph[node]:
+            if next_node not in path:
+                new_path = path + [next_node]
+                queue.append((next_node, new_path))
+                if next_node == end:
+                    return new_path
